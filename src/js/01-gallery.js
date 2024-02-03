de2242e7579f3c2aa3c56b1afedd00faf84aac9a
KrolLew23 <aniabandola@gmail.com>
2024-02-03 03:58:57 +0100

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryElement = document.querySelector(".gallery");
+
+const galleryList = galleryItems
+  .map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `)
+  .join("");
+
+galleryElement.insertAdjacentHTML("beforeend", galleryList);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
