566ea720c36cc3b9adacec2abf517e136310ac8b
KrolLew23 <aniabandola@gmail.com>
2024-02-04 14:54:25 +0100

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const video = document.querySelector('iframe');
+const player = new VimeoPlayer(video);
+const localStorageKey = 'videoplayer-current-time';
+
+const saveCurrentTimeToLocalStorage = throttle((currentTime) => {
+  localStorage.setItem(localStorageKey, currentTime);
+}, 1000);
+
+player.on('timeupdate', () => {
+  const currentTime = player.currentTime();
+  saveCurrentTimeToLocalStorage(currentTime);
+});
+
+try {
+  const storedTime = parseFloat(localStorage.getItem(localStorageKey)) || 0;
+  player.setCurrentTime(storedTime);
+} catch (error) {
+  console.error('Error:', error);
+}
